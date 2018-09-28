8b834943c8cd58242caa21775475711be4d904fa
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-28 04:50:35 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-   "array".shift
+   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -47,4 +47,3 @@ puts remove_element_from_start_of_array(four)
 puts add_element_to_end_of_array(pizza, toppings)
 puts add_element_to_start_of_array(basketball, items)
 puts remove_element_from_start_of_array(pizza)
-  
