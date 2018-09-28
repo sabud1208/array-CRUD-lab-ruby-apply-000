1a16c99d7ffeeab2557414df59733aa3723c759a
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-28 05:27:59 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,8 +16,8 @@ pizza= ["deep", "thin", "regular", "no sauce"]
 toppings= "Pepperoni"
 
 def add_element_to_start_of_array(array, element)
-  array.unshift
-  array.inspect
+  array.unshift= ("Steph")
+
 end
 basketball= ["ball", "hoops", "score", "timeout"]
 items= "net"
