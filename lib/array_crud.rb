d1963cfce9a9d8272d060263ed63aa8f7a1ca2de
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-28 04:09:26 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 four= ["I", "Love", "Pepperoni", "Pizza"]
 type= "pie"
 
-def add_element_to_end_of_array(array <<, element)
+def add_element_to_end_of_array(array << "element")
    puts four.push "element"
 
 end
