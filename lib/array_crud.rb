0a02cd9ba01a8d019ea56ecddc3cf2bb1f3d2f79
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-28 04:43:23 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  puts four[3]
+  puts array[3]
 end
 
 def retrieve_first_element_from_array(array)
