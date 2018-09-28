5ba8c86e9b7fa003663477812054994257c1c4e3
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-28 05:56:34 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,7 @@ toppings= "Pepperoni"
 
 
 def add_element_to_start_of_array(array, element)
-  array.unshift, ["element"]
-  array.inspect
+  array.unshift(1)
 end
 
 Beyonce= ["Pretty, singer, dancer, mother"]
