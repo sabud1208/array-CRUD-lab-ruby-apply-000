62c153e0e64b6ac4b95d7f27fe82c6e08d70f4ba
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-28 04:08:10 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+  four= ["I", "Love", "Pepperoni", "Pizza"]
 end
 
-def add_element_to_end_of_array(array, element)
+four= ["I", "Love", "Pepperoni", "Pizza"]
+type= "pie"
+
+def add_element_to_end_of_array(array << element)
+   puts four.push "element"
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  puts four.unshift "Ball"
 end
 
 def remove_element_from_end_of_array(array)
-  
+  puts "#{array.pop}"
 end
 
 def remove_element_from_start_of_array(array)
-  
+  puts "#{array.shift}"
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  puts four[3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  puts four.shirt
 end
 
 def retrieve_last_element_from_array(array)
-  
+  four.last
 end
+
+puts create_an_array
+puts remove_element_from_end_of_array(four)
+puts remove_element_from_start_of_array(four)
