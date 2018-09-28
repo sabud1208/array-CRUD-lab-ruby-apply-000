be98796f828b7695ce531f03a9b7731d734c347c
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-28 04:33:41 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
   puts array.unshift ("element")
 end
 basketball= ["ball", "hoops", "score", "timeout"]
-items= net
+items= "net"
 def remove_element_from_end_of_array(array)
   puts "#{array.pop}"
 end
