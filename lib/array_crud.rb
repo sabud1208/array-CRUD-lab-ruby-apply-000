c0be394fe5e0bde1a94973ce836f16186d09543d
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-28 05:07:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,6 +17,7 @@ toppings= "Pepperoni"
 
 def add_element_to_start_of_array(array, element)
   array.unshift("element")
+  array.inspect
 end
 basketball= ["ball", "hoops", "score", "timeout"]
 items= "net"
